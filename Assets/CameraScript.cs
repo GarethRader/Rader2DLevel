6bb8094e026de36a45da9adfe756811965ad4cf8
Gareth Rader <garethm.rader@gmail.com>
2020-10-21 03:43:46 -0600

    First commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraScript : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+        
+    void Update()
+    {   
+        HandleMovement();
+    }
+    private void HandleMovement(){
+        Vector3 cameraFollowPosition = player.position;
+        cameraFollowPosition.z = transform.position.z;
+
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(cameraFollowPosition, this.transform.position);
+        float cameraMoveSpeed = 1f;
+        if(distance > 0){
+            Vector3 newCameraPosition = this.transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+             
+            float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
+
+            if(distanceAfterMoving > distance){
+                // overshot target
+                newCameraPosition = cameraFollowPosition;
+            }
+
+            transform.position = newCameraPosition;
+        }
+    }
+ 
+    
+}
---
+++ Process/README.md
@@ -0,0 +1,8 @@
+Design Goals:
+
+- create emotions of frustration and accomplishment with player progress
+        - similar to jump king
+        - jumping puzzles require precision and timing to get through which can be difficult at times
+        - can "lose" progress if incorrect jump is made and the player has to go through parts they have already done....
+
+I also designed the level with iterations/variations in mind starting from simple jumping puzzles and doing harder variations on them. I am hoping to find the balance of difficult and not too easy which I think is a challenge for many jumping puzzle games.
